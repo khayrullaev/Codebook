6368554fb458b9c3e91706b8c56e1e48bece798d
alisher <khayrullaev.ali@gmail.com>
2021-10-05 10:00:38 +0900

    setup

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,7 @@ import React from "react";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>This is the app.tsx file</p>
-      </header>
+      <p>This is home page</p>
     </div>
   );
 }
