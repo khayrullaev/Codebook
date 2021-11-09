9a2695d60b460a3a0780aa2943594fd26074878d
alisher <khayrullaev.ali@gmail.com>
2021-11-09 09:05:59 +0900

    code hiighlight component added

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,64 @@
 import { makeStyles } from '@mui/styles';
 import Page from 'components/Page';
 import { Typography } from '@mui/material';
+import Snippet from 'components/common/Snippet';
+
+const exampleCode = `import React from 'react';
+import { Container, Box } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+import { CopyBlock, dracula } from 'react-code-blocks';
+
+type SnippetProps = {
+  code: string;
+  language: string;
+  theme: string;
+};
+
+const Snippet = ({ code, language, theme }: SnippetProps) => {
+  const classes = useStyles();
+
+  return (
+    <Box className={classes.root}>
+      <Container>
+        <CopyBlock
+          //style={{ width: '100%', height: '100%' }}
+          text={code}
+          language={language}
+          showLineNumbers={true}
+          theme={dracula}
+        />
+      </Container>
+    </Box>
+  );
+};
+
+const useStyles = makeStyles((theme: any) => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+    border: '1px solid red',
+    width: 500,
+    height: 'auto',
+  },
+  copyBlock: {
+    width: '100%',
+    height: '100%',
+  },
+}));
+
+export default Snippet;`;
 
 export default function HomePage() {
   const classes = useStyles();
 
   return (
     <Page title="HomePage" className={classes.root}>
-      <Typography color="textPrimary">Home Page</Typography>
+      {/* <Typography color="textPrimary">Home Page</Typography> */}
+      <Snippet
+        code={exampleCode}
+        language={'javascript'}
+        userName="khayrullaev"
+      />
     </Page>
   );
 }
@@ -15,5 +66,6 @@ export default function HomePage() {
 const useStyles = makeStyles((theme: any) => ({
   root: {
     backgroundColor: theme.palette.background.light,
+    padding: theme.spacing(2),
   },
 }));
